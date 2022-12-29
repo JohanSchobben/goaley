6e014c01a3adf69fb5d596a76375eb6239728a3e
Johan Schobben <johanschobben@gmail.com>
2022-12-29 19:17:57 +0100

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,9 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+
+const routes: RouteRecordRaw[] = [];
+
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
+});
